2b070eb9743cce1310cd6a8fbc9b5796e5ca44ef
Silvia0812 <smdonatellix@gmail.com>
2026-08-16 20:54:59 +0200

    Adiciona testes unitarios customizados para models do forum

---
+++ tests/unit/forum/test_forum_models_custom.py
@@ -0,0 +1,124 @@
+import pytest
+from flaskbb.forum.models import Category, Forum, Topic, Post
+
+
+def test_criar_categoria_com_sucesso(database):
+    """Testa a criacao direta de uma categoria no banco de dados."""
+    category = Category(title="Categoria Teste", position=1).save()
+
+    assert category.id is not None
+    assert category.title == "Categoria Teste"
+
+
+def test_criar_forum_pertencente_a_categoria(database):
+    """Testa a criacao de um forum associado a uma categoria existente."""
+    category = Category(title="Geral", position=1).save()
+
+    forum = Forum(
+        title="Forum de Discussoes",
+        description="Descricao do forum",
+        category_id=category.id,
+        position=1,
+    ).save()
+
+    assert forum.id is not None
+    assert forum.category_id == category.id
+
+
+def test_criar_topico_e_validar_post_inicial(database, forum, user):
+    """Testa a criacao de um topico atribuindo atributos manualmente."""
+    topic = Topic()
+    topic.title = "Topico de Teste"
+    topic.forum_id = forum.id
+    topic.user_id = user.id
+    topic.username = user.username
+    database.session.add(topic)
+    database.session.commit()
+
+    post = Post()
+    post.content = "Conteudo do post de teste"
+    post.topic_id = topic.id
+    post.user_id = user.id
+    post.username = user.username
+    database.session.add(post)
+    database.session.commit()
+
+    topic.first_post_id = post.id
+    topic.last_post_id = post.id
+    database.session.commit()
+
+    assert topic.id is not None
+    assert topic.first_post_id == post.id
+
+
+def test_exclusao_de_topico(database, forum, user):
+    """Testa a remocao de um topico do banco de dados."""
+    topic = Topic()
+    topic.title = "Para Deletar"
+    topic.forum_id = forum.id
+    topic.user_id = user.id
+    topic.username = user.username
+    database.session.add(topic)
+    database.session.commit()
+
+    topic_id = topic.id
+    database.session.delete(topic)
+    database.session.commit()
+
+    assert database.session.get(Topic, topic_id) is None
+
+
+def test_travar_e_destravar_topico(database, forum, user):
+    """Testa a alteracao de status de trancamento de um topico."""
+    topic = Topic()
+    topic.title = "Topico Trancado"
+    topic.forum_id = forum.id
+    topic.user_id = user.id
+    topic.username = user.username
+    topic.locked = True
+    database.session.add(topic)
+    database.session.commit()
+
+    assert topic.locked is True
+
+    topic.locked = False
+    database.session.commit()
+    assert topic.locked is False
+
+
+def test_fixar_topico(database, forum, user):
+    """Testa se um topico pode ser marcado como importante/fixo."""
+    topic = Topic()
+    topic.title = "Aviso Importante"
+    topic.forum_id = forum.id
+    topic.user_id = user.id
+    topic.username = user.username
+    topic.important = True
+    database.session.add(topic)
+    database.session.commit()
+
+    assert topic.important is True
+
+
+def test_edicao_de_post(database, forum, user):
+    """Testa a atualizacao do conteudo de um post."""
+    topic = Topic()
+    topic.title = "Topico para Edicao"
+    topic.forum_id = forum.id
+    topic.user_id = user.id
+    topic.username = user.username
+    database.session.add(topic)
+    database.session.commit()
+
+    post = Post()
+    post.content = "Texto original"
+    post.topic_id = topic.id
+    post.user_id = user.id
+    post.username = user.username
+    database.session.add(post)
+    database.session.commit()
+
+    post.content = "Texto editado com sucesso"
+    database.session.commit()
+
+    assert post.content == "Texto editado com sucesso"
